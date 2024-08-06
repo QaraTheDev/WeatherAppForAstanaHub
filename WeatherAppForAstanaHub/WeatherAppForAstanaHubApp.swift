237b5e9d39aba8ba2fb543bfce783cda0f486654
QaraTheDev <andas.salgara@mail.ru>
2024-08-06 18:47:37 +0500

    first commit

---
+++ WeatherAppForAstanaHub/WeatherAppForAstanaHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppForAstanaHubApp.swift
+//  WeatherAppForAstanaHub
+//
+//  Created by Andas on 06.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppForAstanaHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
